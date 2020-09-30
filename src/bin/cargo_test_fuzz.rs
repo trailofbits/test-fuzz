3f1ccfc32c80e49d8ed38979d27de29aff213839
Samuel E. Moelius III <sam@moeli.us>
2020-09-30 19:19:36 -0400

    Add exact option

--- src/bin/cargo_test_fuzz.rs
+++ src/bin/cargo_test_fuzz.rs
@@ -32,6 +32,7 @@ enum SubCommand {
     TestFuzz(TestFuzz),
 }
 
+// smoelius: Wherever possible, try to reuse cargo test and libtest option names.
 #[derive(Clap, Debug)]
 struct TestFuzz {
     #[clap(long, about = "Display backtraces")]
@@ -48,6 +49,8 @@ use --display-corpus-instrumented"
     display_crashes: bool,
     #[clap(long, about = "Display work queue")]
     display_queue: bool,
+    #[clap(long, about = "Target name is an exact name rather than a substring")]
+    exact: bool,
     #[clap(long, about = "List fuzz targets")]
     list: bool,
     #[clap(long, about = "Resume target's last fuzzing session")]
@@ -99,7 +102,7 @@ fn main() -> Result<()> {
     let mut executable_targets = executable_targets(&executables)?;
 
     if let Some(pat) = &opts.target {
-        executable_targets = filter_executable_targets(&pat, &executable_targets);
+        executable_targets = filter_executable_targets(&opts, &pat, &executable_targets);
     }
 
     if opts.list {
@@ -232,13 +235,14 @@ fn targets(executable: &PathBuf) -> Result<Vec<String>> {
 }
 
 fn filter_executable_targets(
+    opts: &TestFuzz,
     pat: &str,
     executable_targets: &[(PathBuf, String, Vec<String>)],
 ) -> Vec<(PathBuf, String, Vec<String>)> {
     executable_targets
         .iter()
         .filter_map(|(executable, krate, targets)| {
-            let targets = filter_targets(pat, targets);
+            let targets = filter_targets(opts, pat, targets);
             if !targets.is_empty() {
                 Some((executable.clone(), krate.clone(), targets))
             } else {
@@ -248,10 +252,10 @@ fn filter_executable_targets(
         .collect()
 }
 
-fn filter_targets(pat: &str, targets: &[String]) -> Vec<String> {
+fn filter_targets(opts: &TestFuzz, pat: &str, targets: &[String]) -> Vec<String> {
     targets
         .iter()
-        .filter(|target| target.contains(pat))
+        .filter(|target| (!opts.exact && target.contains(pat)) || target.as_str() == pat)
         .cloned()
         .collect()
 }
@@ -262,14 +266,16 @@ fn executable_target(
 ) -> Result<(PathBuf, String, String)> {
     let mut executable_targets = executable_targets.to_vec();
 
-    ensure!(!executable_targets.is_empty(), "found no fuzz targets");
+    ensure!(
+        !executable_targets.is_empty(),
+        "found no fuzz targets{}",
+        match_message(opts)
+    );
 
     ensure!(
         executable_targets.len() <= 1,
         "found multiple executables with fuzz targets{}: {:#?}",
-        opts.target
-            .as_ref()
-            .map_or("".to_owned(), |pat| format!(" matching `{}`", pat)),
+        match_message(opts),
         executable_targets
     );
 
@@ -280,9 +286,7 @@ fn executable_target(
     ensure!(
         executable_targets.2.len() <= 2,
         "found multiple fuzz targets{} in {:?}: {:#?}",
-        opts.target
-            .as_ref()
-            .map_or("".to_owned(), |pat| format!(" matching `{}`", pat)),
+        match_message(opts),
         (executable_targets.0, executable_targets.1),
         executable_targets.2
     );
@@ -292,6 +296,16 @@ fn executable_target(
         executable_targets.1,
         executable_targets.2.remove(0),
     ))
+}
+
+fn match_message(opts: &TestFuzz) -> String {
+    opts.target.as_ref().map_or("".to_owned(), |pat| {
+        format!(
+            " {} `{}`",
+            if opts.exact { "equal to" } else { "containing" },
+            pat
+        )
+    })
 }
 
 #[derive(Eq, PartialEq)]
