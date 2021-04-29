ff104e6f2a6a6021d84f612f32c37bf55724e64b
Samuel E. Moelius III <sam@moeli.us>
2021-04-29 09:13:20 -0400

    `include_in_production` -> `enable_in_production`

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     target("Hello, world!");
 }
 
-#[test_fuzz::test_fuzz(include_in_production)]
+#[test_fuzz::test_fuzz(enable_in_production)]
 fn target(s: &str) {
     println!("{}", s);
 }
