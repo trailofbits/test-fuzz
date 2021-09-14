3f6873bb6b33f3145cf20f6cc5b8951b59db529e
Samuel E. Moelius III <sam@moeli.us>
2021-09-14 04:06:35 -0400

    Ensure README is up to date

---
+++ scripts/update_README.sh
@@ -0,0 +1,74 @@
+#! /bin/bash
+
+set -euo pipefail
+
+if [[ $# -ne 0 ]]; then
+    echo "$0: expect no arguments" >&2
+    exit 1
+fi
+
+cd "$(dirname "$0")"/..
+
+USAGE="$(mktemp)"
+ARGS="$(mktemp)"
+FLAGS="$(mktemp)"
+OPTIONS="$(mktemp)"
+README="$(mktemp)"
+
+cargo run -p cargo-test-fuzz -- test-fuzz --help |
+sed -n '/^USAGE:/,/^ARGS:/p' |
+tail -n +2 |
+head -n -2 |
+cat > "$USAGE"
+
+cargo run -p cargo-test-fuzz -- test-fuzz --help |
+sed -n '/^ARGS:/,/^FLAGS:/p' |
+tail -n +2 |
+head -n -2 |
+cat > "$ARGS"
+
+cargo run -p cargo-test-fuzz -- test-fuzz --help |
+sed -n '/^FLAGS:/,/^OPTIONS:/p' |
+tail -n +2 |
+head -n -2 |
+cat > "$FLAGS"
+
+cargo run -p cargo-test-fuzz -- test-fuzz --help |
+sed -n '/^OPTIONS:/,$ p' |
+tail -n +2 |
+cat > "$OPTIONS"
+
+NEXT=
+
+IFS=''
+cat README.md |
+while read -r X; do
+    if [[ "$NEXT" = '/dev/zero' ]]; then
+        if [[ "$X" = '```' ]]; then
+            NEXT=
+        else
+            continue
+        fi
+    fi
+
+    echo "$X"
+
+    if [[ "$X" = '#### Usage' ]]; then
+        NEXT='USAGE'
+    elif [[ "$X" = '#### Args' ]]; then
+        NEXT='ARGS'
+    elif [[ "$X" = '#### Flags' ]]; then
+        NEXT='FLAGS'
+    elif [[ "$X" = '#### Options' ]]; then
+        NEXT='OPTIONS'
+    elif [[ "$X" = '```' ]]; then
+        # smoelius: It should not be possible to get here with "$NEXT" = 'dev/zero'.
+        if [[ -n "$NEXT" ]]; then
+            cat "${!NEXT}"
+            NEXT='/dev/zero'
+        fi
+    fi
+done |
+cat > "$README"
+
+mv "$README" README.md
