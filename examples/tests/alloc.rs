68bbe72e2e3377c2a064045cd734a758840b71f4
Samuel E. Moelius III <sam@moeli.us>
2021-10-16 20:54:32 -0400

    Update `alloc` test
    Switching to LLVM 13 (nightly-2021-08-22) was causing the `alloc` test
    to fail. I think the vector allocation was being optimized away because
    printing out the vector's address causes the test to again pass.

--- examples/tests/alloc.rs
+++ examples/tests/alloc.rs
@@ -1,7 +1,9 @@
 mod alloc {
     #[test_fuzz::test_fuzz(no_auto_generate)]
     fn target(n: usize) {
-        let _vec = Vec::<u8>::with_capacity(n);
+        let vec = Vec::<u8>::with_capacity(n);
+        // smoelius: Prevent `vec` from being optimized away.
+        println!("{:p}", &vec);
     }
 
     #[test]
