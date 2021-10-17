224b4a2ac3c4663af6741acfa18983bd80d89333
Samuel E. Moelius III <sam@moeli.us>
2021-10-17 06:29:06 -0400

    Fix Substrate tests
    There was a race between the two tests because
    update_substrate_node_template_patch.sh was overwriting the patch.

--- cargo-test-fuzz/tests/substrate.rs
+++ cargo-test-fuzz/tests/substrate.rs
@@ -1,6 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
-use std::{fs::File, io::Read, path::Path};
+use std::{fs::read_to_string, path::Path};
 use tempfile::tempdir_in;
 
 const SUBSTRATE_NODE_TEMPLATE_URL: &str =
@@ -20,22 +20,14 @@ fn do_something() {
         .assert()
         .success();
 
-    // smoelius: I was getting sporadic `unrecognized input` errors from `git apply` in CI. I don't
-    // know why. Writing the patch directly to `git apply`'s standard input seems to make the
-    // problem go away.
-    // smoelius: The problem persists. For some reason, the patch appears empty, even though
-    // `read_to_string` succeeds. I am going to see if using `File::read_to_end` makes a difference.
-    let patch_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("substrate_node_template.patch");
-    let mut file = File::open(patch_path).unwrap();
-    let mut buf = Vec::new();
-    let _ = file.read_to_end(&mut buf).unwrap();
-    let patch = String::from_utf8(buf).unwrap();
-    println!("{}", patch);
+    let patch = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("substrate_node_template.patch")
+        .canonicalize()
+        .unwrap();
 
     Command::new("git")
         .current_dir(tempdir.path())
-        .args(&["apply"])
-        .write_stdin(patch)
+        .args(&["apply", &patch.to_string_lossy().to_string()])
         .assert()
         .success();
 
@@ -64,25 +56,31 @@ fn do_something() {
 
 #[test]
 fn patch_is_current() {
-    let parent = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap();
-
-    Command::new(
-        parent
-            .join("scripts")
-            .join("update_substrate_node_template_patch.sh"),
-    )
-    .current_dir(parent)
-    .assert()
-    .success();
+    let tempdir = tempdir_in(env!("CARGO_MANIFEST_DIR")).unwrap();
 
     Command::new("git")
-        .current_dir(parent)
-        .args(&[
-            "diff",
-            "--exit-code",
-            "--",
-            "update_substrate_node_template.patch",
-        ])
+        .current_dir(tempdir.path())
+        .args(&["clone", SUBSTRATE_NODE_TEMPLATE_URL, "."])
         .assert()
         .success();
+
+    let patch_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("substrate_node_template.patch");
+    let patch = read_to_string(patch_path).unwrap();
+
+    Command::new("git")
+        .current_dir(tempdir.path())
+        .args(&["apply"])
+        .write_stdin(patch.as_bytes())
+        .assert()
+        .success();
+
+    let assert = Command::new("git")
+        .current_dir(tempdir.path())
+        .args(&["diff"])
+        .assert()
+        .success();
+
+    let diff = String::from_utf8_lossy(&assert.get_output().stdout);
+
+    assert_eq!(patch, diff);
 }
