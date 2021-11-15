cb049a55351a140c42f65a385c0411453a64e6c2
Samuel E. Moelius III <sam@moeli.us>
2021-11-14 19:53:18 -0500

    Use `serde`'s serialize/deserialize implementations for `Arc`

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -3,7 +3,6 @@
 #![cfg_attr(feature = "__auto_concretize", feature(proc_macro_span))]
 
 use darling::FromMeta;
-use if_chain::if_chain;
 use internal::serde_format;
 use lazy_static::lazy_static;
 use proc_macro::TokenStream;
@@ -790,35 +789,16 @@ fn map_typed_arg(
 }
 
 fn map_path_arg(
-    conversions: &Conversions,
+    _conversions: &Conversions,
     i: &Literal,
     expr: &Expr,
     path: &TypePath,
 ) -> (Type, Expr, Expr) {
-    if_chain! {
-        if let Some(PathArguments::AngleBracketed(args)) = type_utils::match_type_path(path, &["std", "sync", "Arc"]);
-        if args.args.len() == 1;
-        if let GenericArgument::Type(ty) = &args.args[0];
-        then {
-            return map_arc_arg(conversions, i, expr, ty);
-        }
-    }
     (
         parse_quote! { #path },
         parse_quote! { #expr.clone() },
         parse_quote! { args.#i },
     )
-}
-
-fn map_arc_arg(
-    conversions: &Conversions,
-    i: &Literal,
-    expr: &Expr,
-    ty: &Type,
-) -> (Type, Expr, Expr) {
-    let expr = parse_quote! { (*#expr) };
-    let (ty, ser, de) = map_typed_arg(conversions, i, &expr, ty);
-    (ty, ser, parse_quote! { std::sync::Arc::new( #de ) })
 }
 
 fn map_ref_arg(
