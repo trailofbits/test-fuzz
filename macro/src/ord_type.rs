1ddbec4a5e1de91e0fdedaf6137090cde78a9d35
Samuel Moelius <sam.moelius@trailofbits.com>
2022-03-10 17:33:10 +0000

    Consider it an error if a conversion does not apply

--- macro/src/ord_type.rs
+++ macro/src/ord_type.rs
@@ -5,21 +5,27 @@ use syn::Type;
 #[derive(Clone)]
 pub struct OrdType(pub Type);
 
+impl std::fmt::Display for OrdType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.to_token_stream().fmt(f)
+    }
+}
+
+impl std::fmt::Debug for OrdType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.to_string().fmt(f)
+    }
+}
+
 impl Ord for OrdType {
     fn cmp(&self, other: &Self) -> Ordering {
-        <String as Ord>::cmp(
-            &self.0.to_token_stream().to_string(),
-            &other.0.to_token_stream().to_string(),
-        )
+        <String as Ord>::cmp(&self.to_string(), &other.to_string())
     }
 }
 
 impl PartialOrd for OrdType {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        <String as PartialOrd>::partial_cmp(
-            &self.0.to_token_stream().to_string(),
-            &other.0.to_token_stream().to_string(),
-        )
+        <String as PartialOrd>::partial_cmp(&self.to_string(), &other.to_string())
     }
 }
 
@@ -27,9 +33,6 @@ impl Eq for OrdType {}
 
 impl PartialEq for OrdType {
     fn eq(&self, other: &Self) -> bool {
-        <String as PartialEq>::eq(
-            &self.0.to_token_stream().to_string(),
-            &other.0.to_token_stream().to_string(),
-        )
+        <String as PartialEq>::eq(&self.to_string(), &other.to_string())
     }
 }
