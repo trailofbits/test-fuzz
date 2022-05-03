90c71469880094a7807c24afc9841fb5acffd1cb
Samuel Moelius <sam.moelius@trailofbits.com>
2022-05-03 11:57:51 +0000

    Add Solana `helloworld` test

--- cargo-test-fuzz/tests/third_party.rs
+++ cargo-test-fuzz/tests/third_party.rs
@@ -1,33 +1,61 @@
 use assert_cmd::Command;
+use bitflags::bitflags;
 use predicates::prelude::*;
-use std::{fs::read_to_string, path::Path};
+use rustc_version::{version_meta, Channel};
+use std::{
+    fs::{read_to_string, OpenOptions},
+    io::Write,
+    path::Path,
+};
 use tempfile::tempdir_in;
 use test_log::test;
 
+bitflags! {
+    struct Flags: u8 {
+        const EXPENSIVE = 0b0000_0001;
+        const REQUIRES_ISOLATION = 0b0000_0010;
+        const SKIP_NIGHTLY = 0b0000_0100;
+    }
+}
+
 struct Test<'a> {
-    expensive: bool,
+    flags: Flags,
     url: &'a str,
     patch: &'a str,
+    subdir: &'a str,
     package: &'a str,
 }
 
 const TESTS: &[Test] = &[
     Test {
-        expensive: true,
+        flags: Flags::EXPENSIVE,
         url: "https://github.com/paritytech/substrate",
         patch: "substrate_client_transaction_pool.patch",
+        subdir: ".",
         package: "sc-transaction-pool",
     },
     Test {
-        expensive: true,
+        // https://github.com/bitflags/bitflags/issues/180#issuecomment-499302965
+        flags: Flags::from_bits_truncate(
+            Flags::REQUIRES_ISOLATION.bits() | Flags::SKIP_NIGHTLY.bits(),
+        ),
+        url: "https://github.com/solana-labs/example-helloworld",
+        patch: "example-helloworld.patch",
+        subdir: "src/program-rust",
+        package: "solana-bpf-helloworld",
+    },
+    Test {
+        flags: Flags::EXPENSIVE,
         url: "https://github.com/solana-labs/solana",
         patch: "solana.patch",
+        subdir: ".",
         package: "solana-bpf-loader-program",
     },
     Test {
-        expensive: false,
+        flags: Flags::empty(),
         url: "https://github.com/substrate-developer-hub/substrate-node-template",
         patch: "substrate_node_template.patch",
+        subdir: ".",
         package: "pallet-template",
     },
 ];
@@ -37,18 +65,28 @@ const LINES_OF_CONTEXT: u32 = 2;
 
 #[test]
 fn cheap_tests() {
+    let version_meta = version_meta().unwrap();
     TESTS
         .iter()
-        .filter(|test| !test.expensive)
+        .filter(|test| {
+            !test.flags.contains(Flags::EXPENSIVE)
+                && (!test.flags.contains(Flags::SKIP_NIGHTLY)
+                    || version_meta.channel != Channel::Nightly)
+        })
         .for_each(run_test);
 }
 
 #[test]
 #[ignore]
 fn expensive_tests() {
+    let version_meta = version_meta().unwrap();
     TESTS
         .iter()
-        .filter(|test| test.expensive)
+        .filter(|test| {
+            test.flags.contains(Flags::EXPENSIVE)
+                && (!test.flags.contains(Flags::SKIP_NIGHTLY)
+                    || version_meta.channel != Channel::Nightly)
+        })
         .for_each(run_test);
 }
 
@@ -75,23 +113,37 @@ fn run_test(test: &Test) {
         .assert()
         .success();
 
+    let subdir = tempdir.path().join(test.subdir);
+
+    if test.flags.contains(Flags::REQUIRES_ISOLATION) {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(subdir.join("Cargo.toml"))
+            .unwrap();
+
+        writeln!(file)
+            .and_then(|_| writeln!(file, "[workspace]"))
+            .unwrap();
+    }
+
     // smoelius: Right now, Substrate's lockfile refers to `pin-project:0.4.27`, which is
     // incompatible with `syn:1.0.84`.
     Command::new("cargo")
-        .current_dir(tempdir.path())
+        .current_dir(&subdir)
         .args(&["update"])
         .assert()
         .success();
 
     Command::new("cargo")
-        .current_dir(tempdir.path())
+        .current_dir(&subdir)
         .args(&["test", "--package", test.package, "--", "--nocapture"])
         .assert()
         .success();
 
     Command::cargo_bin("cargo-test-fuzz")
         .unwrap()
-        .current_dir(tempdir.path())
+        .current_dir(&subdir)
         .args(&["test-fuzz", "--package", test.package, "--display=corpus"])
         .assert()
         .success()
@@ -99,7 +151,7 @@ fn run_test(test: &Test) {
 
     Command::cargo_bin("cargo-test-fuzz")
         .unwrap()
-        .current_dir(tempdir.path())
+        .current_dir(&subdir)
         .args(&["test-fuzz", "--package", test.package, "--replay=corpus"])
         .assert()
         .success()
