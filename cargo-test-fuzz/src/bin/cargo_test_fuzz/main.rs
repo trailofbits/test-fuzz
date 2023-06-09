62e31a80958e1693bf2d3a766d5eb28f826f95f6
Samuel Moelius <samuel.moelius@trailofbits.com>
2023-06-09 12:53:44 +0000

    Officially deprecate the below
    - Options `--display-*`
    - Options `--replay-*`
    - Option `--target`
    - Public function `cargo_test_fuzz`

---
+++ cargo-test-fuzz/src/bin/cargo_test_fuzz/main.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use cargo_test_fuzz::{run, Object, TestFuzz};
+use std::env;
+use std::ffi::OsString;
+
+mod transition;
+use transition::cargo_test_fuzz;
+
+pub fn main() -> Result<()> {
+    env_logger::init();
+
+    let args: Vec<_> = env::args().map(OsString::from).collect();
+
+    cargo_test_fuzz(&args)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::cargo_test_fuzz as cargo;
+    use anyhow::Result;
+
+    #[cfg_attr(
+        dylint_lib = "non_thread_safe_call_in_test",
+        allow(non_thread_safe_call_in_test)
+    )]
+    #[test]
+    fn build_no_instrumentation_with_target() {
+        #[allow(clippy::unwrap_used)]
+        cargo_test_fuzz(&[
+            "--features",
+            &("test-fuzz/".to_owned() + test_fuzz::serde_format().as_feature()),
+            "--no-run",
+            "--no-instrumentation",
+            "target",
+        ])
+        .unwrap();
+    }
+
+    fn cargo_test_fuzz(args: &[&str]) -> Result<()> {
+        let mut cargo_args = vec!["cargo-test-fuzz", "test-fuzz"];
+        cargo_args.extend_from_slice(args);
+        cargo(&cargo_args)
+    }
+}
