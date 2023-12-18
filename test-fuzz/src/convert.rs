52183cb40b32b2c477a45c1058edf7d2167ae498
Samuel Moelius <samuel.moelius@trailofbits.com>
2023-12-18 10:38:18 -0500

    Add note to `test_fuzz::Into`

--- test-fuzz/src/convert.rs
+++ test-fuzz/src/convert.rs
@@ -12,6 +12,23 @@ where
     }
 }
 
+/// Trait whose implementation is required by the [`test_fuzz` macro]'s [`convert`] option.
+///
+/// The reason for using a non-standard trait is to avoid conflicts that could arise from blanket
+/// implementations of standard traits. For example, trying to use `From` instead of
+/// `test_fuzz::Into` can lead to errors like the following:
+///
+/// ```text
+/// conflicting implementation in crate `core`:
+/// - impl<T> From<T> for T;
+/// ```
+///
+/// Such errors were observed in the [Substrate Node Template] [third-party test].
+///
+/// [`convert`]: https://github.com/trailofbits/test-fuzz/blob/master/README.md#convert--x-y
+/// [`test_fuzz` macro]: https://github.com/trailofbits/test-fuzz/blob/master/README.md#test_fuzz-macro
+/// [Substrate Node Template]: https://github.com/trailofbits/test-fuzz/blob/master/cargo-test-fuzz/patches/substrate_node_template.patch
+/// [third-party test]: https://github.com/trailofbits/test-fuzz/blob/master/cargo-test-fuzz/tests/third_party.rs
 pub trait Into<T> {
     fn into(self) -> T;
 }
