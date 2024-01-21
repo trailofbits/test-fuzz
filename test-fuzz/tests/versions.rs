72536e32895173110c66e34c6a363d6e0119964b
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-01-20 21:01:22 -0500

    Remove `readme_references_current_version` test

--- test-fuzz/tests/versions.rs
+++ test-fuzz/tests/versions.rs
@@ -1,17 +1,8 @@
 use cargo_metadata::{Dependency, DependencyKind, Metadata, MetadataCommand};
 use once_cell::sync::Lazy;
-use regex::{Match, Regex};
 use semver::Version;
-use std::{
-    fs::read_to_string,
-    path::{Path, PathBuf},
-};
 
 static METADATA: Lazy<Metadata> = Lazy::new(|| MetadataCommand::new().no_deps().exec().unwrap());
-
-#[allow(unknown_lints, env_cargo_path)]
-static README_PATH: Lazy<PathBuf> =
-    Lazy::new(|| Path::new(env!("CARGO_MANIFEST_DIR")).join("../README.md"));
 
 #[test]
 fn versions_are_equal() {
@@ -68,24 +59,5 @@ fn afl_version_is_exact() {
                 );
             }
         }
-    }
-}
-
-#[test]
-fn readme_references_current_version() {
-    let re = Regex::new(r#"(test-fuzz|version) = "([^"]*)""#).unwrap();
-    let content = read_to_string(&*README_PATH).unwrap();
-    for group in re.captures_iter(&content) {
-        let matches: Vec<Option<&str>> = group
-            .iter()
-            .map(|match_| match_.as_ref().map(Match::as_str))
-            .collect();
-        let version = Version::parse(env!("CARGO_PKG_VERSION")).unwrap();
-        assert_eq!(
-            matches.last(),
-            Some(&Some(
-                format!("{}.{}", version.major, version.minor).as_str()
-            ))
-        );
     }
 }
