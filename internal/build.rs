5bda6c3a1cb46aeadf52e9ab4a9d692f1bf1e1db
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-04-11 15:18:10 +0000

    Add `postcard` as a Serde format

--- internal/build.rs
+++ internal/build.rs
@@ -2,7 +2,8 @@ fn main() {
     #[cfg(not(any(
         feature = "__serde_bincode",
         feature = "__serde_cbor",
-        feature = "__serde_cbor4ii"
+        feature = "__serde_cbor4ii",
+        feature = "__serde_postcard"
     )))]
     println!("cargo:rustc-cfg=serde_default");
 }
