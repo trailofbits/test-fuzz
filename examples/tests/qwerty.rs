d522089877849dce9a251ea5cad5a4f6ea7c7088
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-07-04 23:27:25 +0000

    Update qwerty.rs

--- examples/tests/qwerty.rs
+++ examples/tests/qwerty.rs
@@ -1,13 +1,17 @@
+#[cfg_attr(dylint_lib = "supplementary", allow(commented_code))]
 #[test_fuzz::test_fuzz]
 fn target(data: &str) {
     assert!(
         !(data.len() == 6
             && data.as_bytes()[0] == b'q'
             && data.as_bytes()[1] == b'w'
-            && data.as_bytes()[2] == b'e'
-            && data.as_bytes()[3] == b'r'
-            && data.as_bytes()[4] == b't'
-            && data.as_bytes()[5] == b'y')
+            && if cfg!(nightly) {
+                u32::from_be_bytes(data.as_bytes()[2..6].try_into().unwrap()) == 0x65727479
+            } else {
+                data.as_bytes()[2] == b'e' && data.as_bytes()[3] == b'r'
+                // && data.as_bytes()[4] == b't'
+                // && data.as_bytes()[5] == b'y')
+            })
     );
 }
 
