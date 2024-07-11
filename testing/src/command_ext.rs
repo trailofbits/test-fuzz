b3a714274aa54ab1380614b256eff0781846f2aa
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-07-11 13:55:07 +0000

    Strip output of ANSI escapes

--- testing/src/command_ext.rs
+++ testing/src/command_ext.rs
@@ -1,5 +1,6 @@
 use assert_cmd::{assert::Assert, Command};
 use log::debug;
+use std::process::Output;
 
 pub trait CommandExt {
     fn logged_assert(&mut self) -> Assert;
@@ -8,7 +9,21 @@ pub trait CommandExt {
 impl CommandExt for Command {
     fn logged_assert(&mut self) -> Assert {
         debug!("{:?}", self);
-        #[allow(clippy::disallowed_methods)]
-        Self::assert(self)
+        let output = self.output().unwrap();
+        Assert::new(output_stripped_of_ansi_escapes(output))
+    }
+}
+
+fn output_stripped_of_ansi_escapes(output: Output) -> Output {
+    #[allow(clippy::disallowed_methods)]
+    let Output {
+        status,
+        stdout,
+        stderr,
+    } = output;
+    Output {
+        status,
+        stdout: strip_ansi_escapes::strip(stdout),
+        stderr: strip_ansi_escapes::strip(stderr),
     }
 }
