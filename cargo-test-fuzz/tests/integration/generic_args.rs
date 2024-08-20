fcd3804b2e8f73fad02cbd0f6c39ce4f9a8071f0
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-08-20 00:12:04 +0000

    Make `self_ty_in_mod_name` the default

--- cargo-test-fuzz/tests/integration/generic_args.rs
+++ cargo-test-fuzz/tests/integration/generic_args.rs
@@ -10,21 +10,21 @@ fn generic() {
     test(
         "generic",
         "test_bound",
-        "target_bound",
+        "struct_target_bound",
         &impl_expected,
         &expected,
     );
     test(
         "generic",
         "test_where_clause",
-        "target_where_clause",
+        "struct_target_where_clause",
         &impl_expected,
         &expected,
     );
     test(
         "generic",
         "test_only_generic_args",
-        "target_only_generic_args",
+        "struct_target_only_generic_args",
         &impl_expected,
         &expected,
     );
