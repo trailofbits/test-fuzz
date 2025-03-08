16800109cdbb4f6c2ac93171a4e395087b8cfb27
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-03-08 20:19:01 +0000

    Eliminate reliance on `retry`
    Also, log using `Display` rather than `Debug` formatting.

--- testing/src/retry.rs
+++ testing/src/retry.rs
@@ -1,31 +1,30 @@
 use log::{error, info, warn};
-use retry::delay::Fixed;
-use std::fmt::Debug;
+use std::fmt::Display;
 
-pub fn retry<O, T, E>(n: u64, operation: O) -> Result<T, retry::Error<E>>
+pub fn retry<O, T, E>(n: u64, operation: O) -> Result<T, E>
 where
-    O: Fn() -> std::result::Result<T, E>,
-    T: Debug,
-    E: Debug,
+    O: Fn() -> Result<T, E>,
+    T: Display,
+    E: Display,
 {
     assert!(n >= 1);
 
-    retry::retry_with_index(Fixed::from_millis(0), |i| {
-        let result = match operation() {
-            Ok(value) => retry::OperationResult::Ok(value),
-            Err(error) => {
-                if i < n {
-                    retry::OperationResult::Retry(error)
-                } else {
-                    retry::OperationResult::Err(error)
-                }
+    for i in 1..=n {
+        let result = operation();
+        match &result {
+            Ok(value) => {
+                info!("{}", value);
+                return result;
             }
-        };
-        match result {
-            retry::OperationResult::Ok(_) => info!("{:#?}", result),
-            retry::OperationResult::Retry(_) => warn!("{:#?}", result),
-            retry::OperationResult::Err(_) => error!("{:#?}", result),
+            Err(error) if i < n => {
+                warn!("{}", error);
+            }
+            Err(error) => {
+                error!("{}", error);
+                return result;
+            }
         }
-        result
-    })
+    }
+
+    unreachable!();
 }
