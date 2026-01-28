b33fee21d86aa56e349fe47de88e2754e309006f
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-01-28 15:37:46 +0000

    Check lcov by parsing it and stripping disambiguators

--- test-fuzz/tests/integration/in_production.rs
+++ test-fuzz/tests/integration/in_production.rs
@@ -72,22 +72,50 @@ fn test(write: bool, n: usize) {
 
     #[cfg(target_os = "linux")]
     if write {
-        linux::generate_coverage();
+        linux::generate_and_check_coverage();
     }
 }
 
 #[cfg(target_os = "linux")]
 mod linux {
     use super::LoggedAssert;
-    use snapbox::{Redactions, assert_data_eq};
-    use std::{fs::read_to_string, path::Path};
+    use lcov::{Reader, Record};
+    use regex::Regex;
+    use rustc_demangle::demangle;
+    use std::{
+        collections::{BTreeMap, BTreeSet},
+        fs::read_to_string,
+        os::unix::ffi::OsStrExt,
+        path::Path,
+        sync::LazyLock,
+    };
     use testing::fuzzable::test_fuzz_all;
 
-    pub fn generate_coverage() {
+    type FunctionCoverage = BTreeMap<String, u64>;
+
+    type LineCoverage = BTreeMap<String, BTreeSet<u32>>;
+
+    static EXPECTED_FUNCTION_COVERAGE: LazyLock<FunctionCoverage> = LazyLock::new(|| {
+        [
+            (String::from("hello_world::main"), 0),
+            (String::from("hello_world::target"), 1),
+        ]
+        .into_iter()
+        .collect()
+    });
+
+    static EXPECTED_LINE_COVERAGE: LazyLock<LineCoverage> = LazyLock::new(|| {
+        [(
+            String::from("fuzzable/src/bin/hello-world.rs"),
+            [10, 11, 12].into_iter().collect(),
+        )]
+        .into_iter()
+        .collect()
+    });
+
+    pub fn generate_and_check_coverage() {
         #[cfg_attr(dylint_lib = "general", allow(abs_home_path))]
         let parent = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap();
-        let expected_sorted_lcov_info =
-            read_to_string(parent.join("snapshots/sorted_hello_world_lcov.info")).unwrap();
 
         let mut command = test_fuzz_all().unwrap();
         // smoelius: As mentioned above, `hello-world` does not write to, e.g.,
@@ -96,19 +124,78 @@ mod linux {
         command.args(["--coverage=corpus", "intermediary", "--exact"]);
         command.logged_assert().success();
 
-        let actual_lcov_info = read_to_string("lcov.info").unwrap();
-        let mut lines = actual_lcov_info.lines().collect::<Vec<_>>();
-        lines.sort();
-        let sorted_actual_lcov_info = lines
-            .into_iter()
-            .map(|s| format!("{s}\n"))
-            .collect::<String>();
+        let lcov_info = read_to_string("lcov.info").unwrap();
+        let (function_coverage, line_coverage) = lcov_demangle(&lcov_info);
 
-        let mut redactions = Redactions::new();
-        redactions.insert("[REPO]", parent).unwrap();
-        assert_data_eq!(
-            redactions.redact(&sorted_actual_lcov_info),
-            expected_sorted_lcov_info
-        );
+        let hello_world_function_coverage = function_coverage
+            .into_iter()
+            .filter(|(name, _)| name.starts_with("hello_world::"))
+            .collect::<FunctionCoverage>();
+        assert_eq!(*EXPECTED_FUNCTION_COVERAGE, hello_world_function_coverage);
+
+        let hello_world_line_coverage = line_coverage
+            .into_iter()
+            .filter_map(|(path, count)| {
+                let stripped = Path::new(&path).strip_prefix(parent).ok()?;
+                if stripped.starts_with("fuzzable") {
+                    Some((stripped.to_string_lossy().to_string(), count))
+                } else {
+                    None
+                }
+            })
+            .collect::<LineCoverage>();
+        assert_eq!(*EXPECTED_LINE_COVERAGE, hello_world_line_coverage);
+    }
+
+    // smoelius: `lcov_demangle` is loosely based on `lcov_read` from `cargo-line-test`:
+    // https://github.com/trailofbits/cargo-line-test/blob/ebe2fb110eaeb2255d919c838edcc078a0147467/src/db/read.rs#L97
+    fn lcov_demangle(lcov: &str) -> (FunctionCoverage, LineCoverage) {
+        let mut function_coverage = FunctionCoverage::default();
+        let mut line_coverage = LineCoverage::default();
+        let mut source_file = None;
+        for result in Reader::new(lcov.as_bytes()) {
+            match result.unwrap() {
+                Record::SourceFile { path } => {
+                    if let Some(source_file) = &source_file {
+                        panic!("source file already given: {source_file}");
+                    }
+                    let path_utf8 = std::str::from_utf8(path.as_os_str().as_bytes()).unwrap();
+                    source_file = Some(path_utf8.to_owned());
+                }
+                Record::LineData {
+                    line,
+                    count,
+                    checksum: _,
+                } if count != 0 => {
+                    let Some(source_file) = &source_file else {
+                        panic!("source file not given");
+                    };
+                    line_coverage
+                        .entry(source_file.clone())
+                        .or_default()
+                        .insert(line);
+                }
+                Record::FunctionData { name, count } => {
+                    let demangled_name = demangle(&name).to_string();
+                    let stripped_demangled_name = strip_disambiguators(&demangled_name);
+                    let existing_count = function_coverage
+                        .entry(stripped_demangled_name)
+                        .or_default();
+                    *existing_count += count;
+                }
+                Record::EndOfRecord => {
+                    source_file = None;
+                }
+                _ => {}
+            }
+        }
+        (function_coverage, line_coverage)
+    }
+
+    static DISAMBIGUATOR_RE: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r"\[[[:xdigit:]]*\]").unwrap());
+
+    fn strip_disambiguators(name: &str) -> String {
+        DISAMBIGUATOR_RE.replace_all(name, "").to_string()
     }
 }
