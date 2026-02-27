2625a114416293a5a4106ce2537e171a402d0ce3
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-02-27 13:00:32 -0500

    Make `postcard` the default

--- cargo-test-fuzz/tests/integration/fuzz_generic.rs
+++ cargo-test-fuzz/tests/integration/fuzz_generic.rs
@@ -8,8 +8,9 @@ const MAX_TOTAL_TIME: &str = "60";
 #[cfg_attr(dylint_lib = "general", allow(non_thread_safe_call_in_test))]
 #[test]
 fn fuzz_foo_qwerty() {
-    // smoelius: When `bincode` is enabled, `cargo-afl` fails because "the program crashed with one
-    // of the test cases provided."
+    // smoelius: Both `bincode` and `postcard` serialize enum variant indexes rather than their
+    // names. This causes `cargo-afl` to fail because "the program crashed with one of the test
+    // cases provided." Hence, the expected exit code is 2 (error) rather than 1 (crash found).
     fuzz("test_foo_qwerty", 2);
 }
 
--- internal/src/serde_format.rs
+++ internal/src/serde_format.rs
@@ -1,13 +1,13 @@
 use serde::{Serialize, de::DeserializeOwned};
 use std::io::Read;
 
-#[cfg(any(serde_default, feature = "__serde_bincode"))]
+#[cfg(feature = "__serde_bincode")]
 const BYTE_LIMIT: usize = 1024 * 1024 * 1024;
 
 // smoelius: I can't find any guidance on how to choose this size. 2048 is used in the `loopback`
 // test in the `postcard` repository:
 // https://github.com/jamesmunns/postcard/blob/03865c2b7d694d000c0457e8cfaf4ff1b128ed81/tests/loopback.rs#L191
-#[cfg(feature = "__serde_postcard")]
+#[cfg(any(serde_default, feature = "__serde_postcard"))]
 const SLIDING_BUFFER_SIZE: usize = 2048;
 
 #[allow(clippy::vec_init_then_push)]
@@ -15,10 +15,10 @@ const SLIDING_BUFFER_SIZE: usize = 2048;
 pub fn as_feature() -> &'static str {
     let mut formats = vec![];
 
-    #[cfg(any(serde_default, feature = "__serde_bincode"))]
+    #[cfg(feature = "__serde_bincode")]
     formats.push("serde_bincode");
 
-    #[cfg(feature = "__serde_postcard")]
+    #[cfg(any(serde_default, feature = "__serde_postcard"))]
     formats.push("serde_postcard");
 
     assert!(
@@ -30,7 +30,7 @@ pub fn as_feature() -> &'static str {
 }
 
 pub fn serialize<T: Serialize>(args: &T) -> Vec<u8> {
-    #[cfg(any(serde_default, feature = "__serde_bincode"))]
+    #[cfg(feature = "__serde_bincode")]
     return {
         // smoelius: From
         // https://github.com/bincode-org/bincode/blob/c44b5e364e7084cdbabf9f94b63a3c7f32b8fb68/src/lib.rs#L102-L103 :
@@ -44,7 +44,7 @@ pub fn serialize<T: Serialize>(args: &T) -> Vec<u8> {
         bincode::serde::encode_to_vec(args, config).unwrap()
     };
 
-    #[cfg(feature = "__serde_postcard")]
+    #[cfg(any(serde_default, feature = "__serde_postcard"))]
     return {
         let mut data = Vec::new();
         postcard::to_io(args, &mut data).unwrap();
@@ -54,13 +54,13 @@ pub fn serialize<T: Serialize>(args: &T) -> Vec<u8> {
 
 #[allow(unused_mut)]
 pub fn deserialize<T: DeserializeOwned, R: Read>(mut reader: R) -> Option<T> {
-    #[cfg(any(serde_default, feature = "__serde_bincode"))]
+    #[cfg(feature = "__serde_bincode")]
     return {
         let config = bincode::config::standard().with_limit::<BYTE_LIMIT>();
         bincode::serde::decode_from_std_read(&mut reader, config).ok()
     };
 
-    #[cfg(feature = "__serde_postcard")]
+    #[cfg(any(serde_default, feature = "__serde_postcard"))]
     return {
         let mut buff = [0; SLIDING_BUFFER_SIZE];
         postcard::from_io((reader, &mut buff))
